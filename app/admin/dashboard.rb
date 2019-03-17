d159fd8be52f2f43ea995c00d3bcd31b03161f03
Katheryne Arancibia <katheryne.arancib.12@sansano.usm.cl>
2019-03-17 11:15:24 -0300

    add active_admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,33 +1,47 @@
 ActiveAdmin.register_page "Dashboard" do
-
+  
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
-
+  
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
+    
+    div do
+      render 'graph', average: average
     end
-
+    
     # Here is an example of a simple dashboard with columns and panels.
     #
     # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
+    # column do
+    # panel "Recent Posts" do
+    # ul do
+    # Post.recent(5).map do |post|
+    # li link_to(post.title, admin_post_path(post))
     # end
-  end # content
+    # end
+    # end
+    # end
+    
+    # column do
+    # panel "Info" do
+    # para "Welcome to ActiveAdmin."
+    # end
+    # end
+    # end
+  end
+end
+
+def average
+  @result=[]
+  User.where(role: :electric).each do |electric|
+    sum = 0
+    electric.electric_qualifications.each do |q|
+      sum += q.calification
+    end
+    inner = []
+    count = electric.electric_qualifications.count
+    prom = sum/((count > 0) ? count : 1)
+    inner += [electric.name.to_s, prom]
+    @result.push(inner)
+  end
+  @result.sort_by { |ed| ed.last }.reverse!.first(10)
 end
